5c351448e6f4325f03185dbc15e887347dc6373e
sukhpaul <sukhpaul.nijjar@gmail.com>
2017-10-25 16:23:50 -0700

    Pass VideoList Entry Tests

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,17 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry video={props.videos[0]} /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry video={props.videos[1]} /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry video={props.videos[2]} /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry video={props.videos[3]} /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry video={props.videos[4]} /></h5></div>
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: window.exampleVideoData
+  videos: React.PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
